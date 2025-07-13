a7ce38eaf9e7f00e62550bd1f5d01b3a1837ec90
Євгенія Швець <evheniia@MacBook-Air-Evgenia.local>
2025-07-13 20:55:01 +0300

    task fix

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
-import { refs } from './js/refs';
-import { addTask, deleteTask, getAllTasks } from './js/tasks';
-import { getFromLS, saveInLS } from './js/local-storage-api';
-import { themeSwitcher } from './js/theme-switcher';
+import { refs } from "./js/refs";
+import { addTask, deleteTask, getAllTasks } from "./js/tasks";
+import { getFromLS, saveInLS } from "./js/local-storage-api";
+import { themeSwitcher } from "./js/theme-switcher";
 
 // 2 - додаємо слухача на форму, забираємо значення з інпутів з перевіркою на пустий рядок
-refs.form.addEventListener('submit', e => {
+refs.form.addEventListener("submit", e => {
   e.preventDefault();
 
   const [taskName, taskDescription] = e.target.elements;
   const title = taskName.value.trim();
   const description = taskDescription.value.trim();
 
-  if (title === '' || description === '') {
-    iziToast.warning({ position: 'center', message: 'Fill all fields please' });
+  if (title === "" || description === "") {
+    iziToast.warning({ position: "center", message: "Fill all fields please" });
     return;
   }
   // створюємо обʼєкт таски для додавання до масива
@@ -30,9 +30,9 @@ refs.form.addEventListener('submit', e => {
 getAllTasks();
 
 // 5 - додаємо слухача на список для видалення таски
-refs.taskList.addEventListener('click', e => {
+refs.taskList.addEventListener("click", e => {
   // перевіряємо що клікаємо саме на кнопку
-  if (e.target.nodeName !== 'BUTTON') return;
+  if (e.target.nodeName !== "BUTTON") return;
   // забираємо заголовок таски де клікнули для видалення
   const titleForDelete = e.target.nextElementSibling.textContent;
   //   передаємо заголовок в функцію видалення
@@ -40,11 +40,13 @@ refs.taskList.addEventListener('click', e => {
 });
 
 // 6 - перевіряємо тему при завантаженні сторінки
-const theme = getFromLS('theme');
-if (theme === 'theme-light') {
-  document.body.classList.add('theme-light');
-  document.body.classList.remove('theme-dark');
+const theme = getFromLS("theme");
+if (theme === "theme-light") {
+  document.body.classList.add("theme-light");
+  document.body.classList.remove("theme-dark");
+} else {
+  saveInLS("theme", "theme-dark");
 }
 
 // 7 - додаємо слухача на кнопку для перемикання теми
-refs.themeSwitcher.addEventListener('click', themeSwitcher);
+refs.themeSwitcher.addEventListener("click", themeSwitcher);
